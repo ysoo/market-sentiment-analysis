6e774a3170a3a19f43adcba7b15336a5cf4f4198
dannysj <csji4202@hotmail.com>
2018-11-10 13:15:49 -0600

    added database connection:

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+const reset = require('./db/schema.js');
+
+reset();
 
 var PORT = process.env.PORT || 3000
 app.listen(PORT, process.env.IP, function() {
