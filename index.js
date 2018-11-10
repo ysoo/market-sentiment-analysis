1903951254e796aa9796ea87fb537f83eae462f7
dannysj <csji4202@hotmail.com>
2018-11-10 14:04:08 -0600

    disable reset

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var app = express();
-const dbreset = require('./server/db/schema.js');
 const db = require('./server/db/db.js');
 
 db.connect();
-dbreset.reset();
 
 var PORT = process.env.PORT || 3000
 app.listen(PORT, process.env.IP, function() {
