3b99b4673de52d137778c18e44d1910e97b600a7
dannysj <csji4202@hotmail.com>
2018-11-10 14:03:04 -0600

    fixed schema

--- server/db/schema.js
+++ server/db/schema.js
@@ -3,6 +3,7 @@ const db = require('./db.js');
 exports.reset = function () {
     const query = `
         DROP TABLE IF EXISTS company_sentinent;
+        DROP TABLE IF EXISTS company_tweets;
         DROP TABLE IF EXISTS company_articles_news;
         DROP TABLE IF EXISTS company;
         
@@ -15,11 +16,20 @@ exports.reset = function () {
             cid int references company(id),
             dob date,
             url text unique,
+            headline text,
             content text
         );
         CREATE TABLE IF NOT EXISTS company_sentinent (
             cid int references company(id),
             sentinent numeric(8,2) default 0.00
+        );
+        CREATE TABLE IF NOT EXISTS company_tweets (
+            id serial unique primary key,
+            cid int references company(id),
+            username varchar(255),
+            dob date,
+            content text,
+            like_count bigint
         );
     `
     db.query(query, function (err, result) {
